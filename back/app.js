4cf14e88bbbdff935de9f2d43647ed8e6a1dc7c8
FluffyNinjaBrick <1111do.gier1111@gmail.com>
2020-02-29 16:07:26 +0100

    backend done

--- back/app.js
+++ back/app.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const Datastore = require('nedb')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//db setup
+const db = new Datastore({filename: './db.json', autoload: true});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//bodyParser setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//get patient info by PESEL
+app.get('/api/:id', (req, res) => {
+    db.find({_id: req.params.id}, function(err, docs) {
+        res.send(docs);
+    });
+});
+
+//update patient
+app.post('/api/:id', (req, res) => {
+    db.update({ _id: req.params.id }, req.body, {}, function (err, numReplaced) {
+       res.send({ status: 'ok' });
+    });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
