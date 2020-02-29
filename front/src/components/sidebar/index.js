277285c7d952b1594a0be17448109afd591e3721
Maciej Kozieja <kozieja.maciej@gmail.com>
2020-02-29 19:35:35 +0100

    side, top bars

--- front/src/components/sidebar/index.js
+++ front/src/components/sidebar/index.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom'
 import './styles.css';
 
-export default class Sidebar extends Component {
+class Sidebar extends Component {
     render() {
+        const { history, location } = this.props
+        const path = location.pathname
         return (
             <div className="sidebarWrapper">
-                <div className="sidebarIcon" onClick={this.navigate("medicine")}></div>
-                <div className="sidebarIcon" onClick={this.navigate("examinations")}></div>
-                <div className="sidebarIcon" onClick={this.navigate("illnesses")}></div>
+                <div className={`sidebarIcon ${/user/.test(path) ? 'active' : ''}`} onClick={() => history.push('')}>
+                    <i className="fas fa-2x fa-user-injured"></i> <br />
+                    Pacjent
+                </div>
+                <div className="sidebarIcon" onClick={() => history.push('')}>
+                    <i className="fas fa-2x fa-pills"></i> <br />
+                    Leki
+                </div>
+                <div className="sidebarIcon" onClick={() => history.push('')}>
+                    <i className="far fa-2x fa-clipboard"></i> <br/>
+                    Badania
+                </div>
             </div>
         )
     }
-
-    search() {
-        //TODO
-    }
-
-    navigate = () => {
-        //TODO
-    }
 }
+export default withRouter(Sidebar)
--- front/src/components/topbar/index.js
+++ front/src/components/topbar/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import './styles.css';
+import { withRouter } from 'react-router-dom';
 
-export default class Topbar extends Component {
+class Topbar extends Component {
     state = { id: '' }
     render() {
         return (
             <div className="topbarWrapper">
                 <input onChange={(val) => this.setState({ id: val.target.value })} type="text" className="idInput" placeholder="Podaj identyfikator"></input>
-                <button onClick={this.search.bind(this)} className="searchButton">Szukaj</button>
+                <button onClick={this.search.bind(this)} className="searchButton">
+                    <i className="fas fa-search"></i>
+                </button>
             </div>
         )
     }
@@ -19,6 +22,7 @@ export default class Topbar extends Component {
             url: '/api/get_uid',
             data: { id: this.state.id }
         }).then(res => res.data)
-        alert(uid)
+        this.props.history.push(`/user/${uid}`)
     }
 }
+export default withRouter(Topbar)
