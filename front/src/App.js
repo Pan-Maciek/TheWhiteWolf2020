c6bb8c4c786cc0f807fcf990107373747a3a3569
Maciej Kozieja <kozieja.maciej@gmail.com>
2020-02-29 16:07:45 +0100

    merged router

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
-import MedicineList from './Components/medicineList';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Topbar from "./Components/topbar";
-import Sidebar from "./Components/sidebar";
+import Topbar from './components/topbar';
+import Sidebar from './components/sidebar'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Topbar/>
-        <MedicineList/>
-    </div>
+    <Router>
+      <Topbar />
+      <div style={{ display: 'flex', flexGrow: 1 }}>
+        <Sidebar />
+        <div>
+          <Switch>
+            <Route path="/user/:uid">User</Route>
+            <Route path="/">Home sweet home!</Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- front/src/components/topbar/index.js
+++ front/src/components/topbar/index.js
@@ -4,9 +4,9 @@ import './styles.css';
 export default class Topbar extends Component {
     render() {
         return (
-            <div id="topbarWrapper">
-                <input type="text" id="idInput" placeholder="Podaj identyfikator"></input>
-                <button onClick={this.search.bind(this)} id="searchButton">Szukaj</button>
+            <div className="topbarWrapper">
+                <input type="text" className="idInput" placeholder="Podaj identyfikator"></input>
+                <button onClick={this.search.bind(this)} className="searchButton">Szukaj</button>
             </div>
         )
     }
