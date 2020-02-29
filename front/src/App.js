f4678efbed223ffa9554b62a0a2a893927ac3a61
Michal23 <michal.wegrzyn2137@gmail.com>
2020-02-29 17:51:55 +0100

    Added exam history

--- front/src/App.js
+++ front/src/App.js
@@ -6,22 +6,22 @@ import MainPatientScreen from './components/mainPatientScreen'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <Topbar />
-      <div style={{ display: 'flex', flexGrow: 1 }}>
-        <Sidebar />
-        <div>
-          <Switch>
-            <Route path="/user/:uid">
-              <MainPatientScreen patient={{name:'Mati', surname:'Obrzut', PESEL: '80110185932'}} />
-            </Route>
-            <Route path="/">Home sweet home!</Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <Topbar />
+            <div style={{ display: 'flex', flexGrow: 1 }}>
+                <Sidebar />
+                <div>
+                    <Switch>
+                        <Route path="/user/:uid">
+                            <MainPatientScreen patient={{name:'Mati', surname:'Obrzut', PESEL: '80110185932'}} />
+                        </Route>
+                        <Route path="/">Home sweet home!</Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
