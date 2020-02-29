93dfdcc0ccd6d2b06b67abf5f0deeac2874279ac
FluffyNinjaBrick <1111do.gier1111@gmail.com>
2020-02-29 13:16:14 +0100

    topbar init

---
+++ front/src/components/topbar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Topbar extends Component {
+    render() {
+        return (
+            <div>
+                <p>henlo</p>
+            </div>
+        )
+    }
+}
